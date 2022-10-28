377a80a676daa729990ecd71c706550fe2e9568d
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-10-28 18:29:24 +0500

    next-js-app

--- pages/burgers/[id].jsx
+++ pages/burgers/[id].jsx
@@ -1,6 +1,38 @@
 import React from 'react'
+import { API_PATH } from '../../tools/constants'
 
-const BurgerId = () => {
+export const getStaticPaths = async () => {
+    const res = await fetch(API_PATH + '/items')
+    const data = await res.json()
+
+    const paths = data.map(burger => {
+        return {
+            params: { id: burger.id }
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async context => {
+    const id = context.params.id
+
+    const res = await fetch(API_PATH + `/items/${id}`)
+    const data = await res.json()
+
+    return {
+        props: {
+            burger: data
+        },
+    }
+
+}
+
+const BurgerId = ({ burger }) => {
+    console.log(burger);
     return (
         <div className='BurgerId'>
             <div className="container">
--- pages/burgers/index.jsx
+++ pages/burgers/index.jsx
@@ -20,7 +20,7 @@ const Burgers = ({ burgers }) => {
             <div className="container">
                 <div className="row">
                     {burgers && burgers.map((item, index) => (
-                        <Link href={`/burger/${item.id}`} key={index} className="col-lg-4 mb-4">
+                        <Link href={`/burgers/${item.id}`} key={index} className="col-lg-4 mb-4">
                             <div className="cards">
                                 <Image src={item.image} width={300} height={300} alt={item.name} />
                                 <h5 className='py-3'>{item.name}</h5>
